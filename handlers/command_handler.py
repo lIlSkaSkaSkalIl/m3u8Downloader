57fc63e46b2d4cf15e81230c9ad98d9514d3d857
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-06-23 23:59:21 +0700

    Update status command_handler.py

--- handlers/command_handler.py
+++ handlers/command_handler.py
@@ -1,9 +1,7 @@
 from pyrogram import filters
 from pyrogram.types import Message
-from pyrogram.handlers import MessageHandler
 
-async def handle_start(_, message: Message):
+start_handler = filters.command("start")
+
+async def start(_, message: Message):
     await message.reply_text("👋 Halo! Kirimkan link m3u8 dan saya akan unduh videonya untukmu.")
-
-# Handler siap dipasang di app
-start_handler = MessageHandler(handle_start, filters.command("start"))
