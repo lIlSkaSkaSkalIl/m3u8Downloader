beefd6186f5b77765533d01244a47414963e7ecc
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-06-24 10:04:09 +0700

    Update upload_handler.py

--- handlers/upload_handler.py
+++ handlers/upload_handler.py
@@ -3,18 +3,18 @@ import time
 from utility.status_format import format_status
 from utils.progress import make_progress_callback
 
-async def upload_video(client, message, status_msg, output_path, filename, flood_lock, duration, thumb):
+async def upload_video(client, message, status_msg, output_path, filename, _, duration, thumb):
     upload_start = time.time()
     last_ul_update = [0]
+    upload_flood_lock = [False]  # ✅ flood_lock terpisah untuk upload
 
-    # Buat callback progres untuk upload
     progress_callback = make_progress_callback(
         client=client,
         status_msg=status_msg,
         label="📤 Mengunggah",
         output_path=output_path,
         start_time=upload_start,
-        flood_lock=flood_lock,
+        flood_lock=upload_flood_lock,
         last_update_ref=last_ul_update
     )
 
