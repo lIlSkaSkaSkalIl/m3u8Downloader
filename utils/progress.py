e707a9d06b410decc28307c95978230f847f6e68
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-06-24 10:21:12 +0700

    Update progress.py

--- utils/progress.py
+++ utils/progress.py
@@ -3,10 +3,10 @@ from pyrogram.errors import FloodWait
 from utility.status_format import format_status
 from math import floor
 
-def make_progress_callback(client, status_msg, label, output_path, start_time, flood_lock, last_update_ref):
+def make_progress_callback(client, status_msg, label, output_path, start_time, last_update_ref):
     async def progress_callback(current, total):
         now = time.time()
-        if now - last_update_ref[0] < 10 or flood_lock[0]:
+        if now - last_update_ref[0] < 10:
             return
         last_update_ref[0] = now
         elapsed = floor(now - start_time)
@@ -20,8 +20,7 @@ def make_progress_callback(client, status_msg, label, output_path, start_time, f
             )
             print(f"[PROGRESS] ✅ Status diperbarui ({label}): {current}/{total}")
         except FloodWait as e:
-            print(f"[PROGRESS] 🚫 FloodWait {e.value}s, menonaktifkan update.")
-            flood_lock[0] = True
+            print(f"[PROGRESS] 🚫 FloodWait {e.value}s")
         except Exception as e:
             print(f"[PROGRESS] ❌ Gagal update: {e}")
     return progress_callback
