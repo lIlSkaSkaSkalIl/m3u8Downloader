87983fb53854783dd06ac00f8c54f54f833438bf
lIlSkaSkaSkalIl <168249736+lIlSkaSkaSkalIl@users.noreply.github.com>
2025-06-24 12:15:25 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from aiogram import Bot, Dispatcher, executor
-from config import BOT_TOKEN as API_TOKEN  # Gunakan BOT_TOKEN dari config.py
+from config import BOT_TOKEN as API_TOKEN
 from handlers.command_handler import register_commands
 from handlers.download_handler import register_download
-from handlers.upload_handler import register_upload
 
 # Inisialisasi bot dan dispatcher
 bot = Bot(token=API_TOKEN)
@@ -11,7 +10,6 @@ dp = Dispatcher(bot)
 # Registrasi handler
 register_commands(dp)
 register_download(dp)
-register_upload(dp)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
